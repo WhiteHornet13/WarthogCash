c24d07b93d2cc5d17a0d679cc4d6438744152fdb
WhiteHornet13 <whitehornet13@protonmail.com>
2026-08-02 17:25:55 +0200

    chore: excluye app/build del control de versiones

--- app/build/tmp/kapt3/stubs/debug/com/warthogcash/presupuesto/ui/mymonths/MonthListAdapterKt.java
+++
@@ -1,8 +0,0 @@
-package com.warthogcash.presupuesto.ui.mymonths;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\n\n\u0000\n\u0002\u0010\b\n\u0002\b\u0003\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0003\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0004"}, d2 = {"TIPO_CABECERA_ANIO", "", "TIPO_PIE_CARGANDO", "TIPO_TARJETA_MES", "app_debug"})
-public final class MonthListAdapterKt {
-    private static final int TIPO_CABECERA_ANIO = 0;
-    private static final int TIPO_TARJETA_MES = 1;
-    private static final int TIPO_PIE_CARGANDO = 2;
-}
